f4edbf24801b675a8514b7e089e671d21a86823d
Lachlan Kemp <me@lachlankemp.com>
2021-09-06 13:24:19 +0930

    testig

--- src/lib/user.js
+++ src/lib/user.js
@@ -4,7 +4,11 @@ import 'gun/axe';
 import { writable } from 'svelte/store';
 
 // Database
-export const db = GUN();
+export const db = GUN({peers: ['https://swagchat-backend-v5-eu.herokuapp.com/gun','http://192.168.194.207:8765/gun', 'http://localhost:8765/gun']});
+
+setInterval(() => {
+    console.dir(db._.opt.peers)
+}, 1000);
 
 // Gun User
 export const user = db.user().recall({sessionStorage: true});
